ce6402295368b5a0ba247c8ceb1779878190e6a2
郑奇 <zq631473076@sina.com>
2017-09-01 11:57:40 +0800

    get datagrid row index function && upload multiple files in servicelist && add row

--- js/common.js
+++ js/common.js
@@ -1,1 +1,9 @@
-var commonURL = "http://115.159.225.127:7070/bjcat／";
+//公共接口调用前缀
+var commonURL = "http://115.159.225.127:7070/bjcat/";
+
+//获取datagrid单选行
+function getRowIndex(selector) {
+    var target = selector.datagrid('getSelected');
+    var rowIndex = selector.datagrid('getRowIndex',target)
+    return rowIndex
+}
