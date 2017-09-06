5f9d363ba834d2a880b4efc81522d64f84711ca2
郑奇 <zq631473076@sina.com>
2017-09-06 18:00:44 +0800

    teachermanagement rid

--- js/common.js
+++ js/common.js
@@ -7,3 +7,55 @@ function getRowIndex(selector) {
     var rowIndex = selector.datagrid('getRowIndex',target)
     return rowIndex
 }
+
+//获取url参数
+function getRequest() {
+    var url = location.search;
+    var theRequest = new Object();
+    if (url.indexOf("?") != -1) {
+        var strs = url.substr(1).split("&");
+        for (var i = 0; i < strs.length; i++) {
+            theRequest[strs[i].split("=")[0]] = strs[i].split("=")[1];
+        }
+    }
+    return theRequest;
+}
+
+function filterNull(value) {
+    if(value === null || value === undefined){
+        return ''
+    }
+    return value
+}
+var useraccount = {
+    sex: {
+        0: '未知',
+        1: '男',
+        2: '女',
+    },
+    usertype: {
+        1: '系统管理员',
+        2: '教职工',
+        3: '学生',
+        4: '校外人员'
+    },
+    census_type: {
+        1: '城镇',
+        2: '农村'
+    },
+    teachtype: {
+        1: '行政管理人员',
+        2: '专职教师',
+        3: '工勤人员'
+    },
+    contractype: {
+        1: '劳动合同',
+        2: '无固定期劳动合同',
+        3: '劳动协议'
+    },
+    workstatus: {
+        0: '离职',
+        1: '在职',
+        2: '休职'
+    }
+}
